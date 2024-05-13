d11f1a449762fced6308917634935caa98818533
Maria-source-cell <manuelabirnat@gmail.com>
2024-05-13 18:28:13 +0200

    Post codealong

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
 
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/post-codealong";
+mongoose.connect(mongoUrl);
+mongoose.Promise = Promise;
+
+const Task = mongoose.model("Task", {
+  text: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  complete: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
+
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
@@ -14,6 +35,30 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.find().sort({ createdAt: "desc" }).limit(20).exec();
+  res.json(tasks);
+});
+
+app.post("/tasks", async (req, res) => {
+  //Retrieve information sent by the client to our API endpoint
+  const { text, complete } = req.body;
+
+  // Use mongoose model to create the database entry
+  const task = new Task({ text, complete });
+
+  try {
+    //Success
+    const savedTask = await task.save();
+    res.status(201).json(savedTask);
+  } catch (err) {
+    res.status(404).json({
+      message: "Could not save task to the Database",
+      error: err.errors,
+    });
+  }
 });
 
 // Start the server
